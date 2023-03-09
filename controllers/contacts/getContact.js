af5792c2ebe904251defdb7b0517ac9c57976546
Vladyslav Hurzhyi <soulline0@gmail.com>
2023-03-09 14:46:21 +0100

    add pagination

--- controllers/contacts/getContact.js
+++ controllers/contacts/getContact.js
@@ -3,7 +3,12 @@ const { HttpError } = require('../../utils');
 
 const getContact = async (req, res) => {
     const { _id: owner } = req.user;
-    const result = await Contact.find({ owner });
+    const { page = 1, limit = 5 } = req.query;
+    const skip = (page - 1) * limit;
+    const result = await Contact.find({ owner }, '', {
+        skip,
+        limit: Number(limit),
+    });
     if (!result) {
         throw HttpError(404, 'Not Found');
     }
